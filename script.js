6ba695c51ed134800dc1991aeae5421d1055a598
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-03-22 19:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //your code here
-
+// document.body.style.backgroundColor = "red";
+let btn = document.getElementById('btn')
+	btn.addEventListener('click',()=>{
+	document.body.style.backgroundColor = "black";
+	document.body.style.color = "white";
+})
